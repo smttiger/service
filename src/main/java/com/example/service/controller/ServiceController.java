b34b184d2d3529ef66363cec3ee793d76c02634e
Siarhei Fabiyanski <siarhei_fabiyanski@epam.com>
2024-12-23 21:02:48 +0100

    add controller

---
+++ src/main/java/com/example/service/controller/ServiceController.java
@@ -0,0 +1,13 @@
+package com.example.service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ServiceController {
+
+    @GetMapping
+    public String getData() {
+        return "Hello world!";
+    }
+}
